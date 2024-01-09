da9ed549cf9986318ac75b5dfd5204bb32446f7b
KLohger <fanimdude32@gmail.com>
2024-01-09 10:59:44 +0100

    fixed and improved example

--- examples/main.rs
+++ examples/main.rs
@@ -2,8 +2,6 @@
 /// This program will increment a counter every time it runs.
 /// be sure to run the executable directly, as ExecutableStorage
 /// assumes the current working is the same the executable is in.
-
-
 use executable_storage::{ExecutableStorage, StaticStorage};
 
 static mut COUNTER: StaticStorage<usize, 7> = StaticStorage {
@@ -11,7 +9,7 @@ static mut COUNTER: StaticStorage<usize, 7> = StaticStorage {
     value: 0,
 };
 fn main() {
-    let mut storage = ExecutableStorage::new(unsafe { &mut STORAGE }).unwrap();
+    let mut storage = ExecutableStorage::new(unsafe { &mut COUNTER }).unwrap();
 
-    *storage = storage.overflowing_add(1).0;
+    *storage = storage.wrapping_add(1);
 }
